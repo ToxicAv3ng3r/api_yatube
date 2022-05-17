6b6f000564383a4c8ab225ef9f806f84b9a49db6
Ilyashenko Andrey <exmoreandrew@gmail.com>
2022-05-17 19:04:56 +0300

    Выполнены правки. Пройдены локальные тесты. Отправка на третье ревью.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -13,7 +13,7 @@ from posts.models import Post, Group, Comment
 class PostListViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -38,7 +38,7 @@ class GroupListViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentListViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
