308ccffc18c59e41a82c72744a74b565bc8f9f5d
Ilyashenko Andrey <exmoreandrew@gmail.com>
2022-05-17 22:47:40 +0300

    Выполнены правки. Пройдены локальные тесты. Отправка на четвертое ревью.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.exceptions import PermissionDenied
 from rest_framework import viewsets, permissions
-from rest_framework.response import Response
-from rest_framework import status
 
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
@@ -17,18 +14,6 @@ class PostListViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(PostListViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        post = get_object_or_404(Post, id=instance.id)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class GroupListViewSet(viewsets.ReadOnlyModelViewSet):
@@ -49,15 +34,3 @@ class CommentListViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user,
                         post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentListViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        comment = get_object_or_404(Comment, id=instance.id)
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
